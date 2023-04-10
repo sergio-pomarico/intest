daa001f12df0ca41c5cb2375f6071561adf9b261
Sergio Pomárico <sergiodavid21@gmail.com>
2023-04-09 21:07:22 -0500

    feat: :art: add custom fonts and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: {
+          100: "#2237D1",
+          200: "#182691",
+          300: "#090F37",
+        },
+      },
+      fontFamily: {
+        be_vietnam_pro: ["Be Vietnam Pro"],
+      },
+    },
+  },
+  plugins: [],
+};
